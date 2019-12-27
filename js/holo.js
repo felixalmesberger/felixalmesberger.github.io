a3c6d93ea6616936ca5ce74fa7b446b1c679ebfb
Felix Almesberger <alf@IAGV.local>
2019-12-27 20:23:48 +0100

    change to lapineta

--- js/holo.js
+++ js/holo.js
@@ -1,16 +1,3 @@
-var myIndex = 0;
-
-function startSlideShow() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(startSlideShow, 9000);    
-}
 
 function updateOpenWidget() {
     $.getJSON('js/opening_times.json', function(data) {
